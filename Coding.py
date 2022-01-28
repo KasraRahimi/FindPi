b48269550d68ba4b97634d3cd36307dd534641ce
jan Kawa <kasra.rahimi@polymtl.ca>
2022-01-27 19:55:57 -0500

    Something which computes pi

--- Coding.py
+++ Coding.py
@@ -1,2 +1,19 @@
-def findPi():
-  pass
+# I will find pi, by finding the area under a half circle
+
+it = int(input("Please define a number of iterations: "))
+
+def HalfCircle(x):
+    return (1-x**2)**(1/2)
+
+def AreaUnderCurve(func, start=0, end=1, it=1):
+    dx = (end - start) / it
+    area = 0
+    for num in range(it):
+        area += dx * func(start + dx * num)
+    return area
+
+AreaOfHalf = AreaUnderCurve(HalfCircle, -1, 1, it)
+
+pi = AreaOfHalf * 2
+
+print(f"Pi is almost: {pi}")
